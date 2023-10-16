9f936e8b3929f9b47cc0b8fb8232c6ff860ef8f8
Mayank vaidya <128823161+mynkkk@users.noreply.github.com>
2023-10-17 03:13:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var rect = document.querySelector(".center");
+
+rect.addEventListener("mousemove" , (details)=>{
+var rectanglelocation = rect.getBoundingClientRect();
+var insiderectval = (details.clientX - rectanglelocation.left);
+
+if(insiderectval<rectanglelocation.width/2){
+    var redcolor = gsap.utils.mapRange(0 , rectanglelocation.width/2 , 255 , 0 , insiderectval);
+    gsap.to(rect, {
+        backgroundColor: `rgb(${redcolor}, 0, 0)`,
+        ease: Power4,
+    })
+    console.log("left")
+}
+else{
+    var bluecolor  = gsap.utils.mapRange(rectanglelocation.width/2, rectanglelocation.width, 0 , 255 , insiderectval);
+    gsap.to(rect,{
+        backgroundColor: `rgb(0, 0 ,${bluecolor})`,
+        ease: Power4,
+    })
+    console.log("right")
+}
+})
+
+rect.addEventListener("mouseleave" , ()=>{
+    gsap.to(rect, {
+        backgroundColor:`white`,
+    })
+})
